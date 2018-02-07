9082e11b761b0846c06d092aa3c53972dac29658
Yuri Tret <yuri.tret@gmail.com>
2018-02-07 16:12:23 +0200

    Passed Login Page

--- SupportTools/Form1.Designer.cs
+++ SupportTools/Form1.Designer.cs
@@ -30,17 +30,21 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPageWifiCache = new System.Windows.Forms.TabPage();
-            this.tabPageSettings = new System.Windows.Forms.TabPage();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.txtUsername = new System.Windows.Forms.TextBox();
-            this.txtPassword = new System.Windows.Forms.TextBox();
-            this.lblUsername = new System.Windows.Forms.Label();
-            this.lblPassword = new System.Windows.Forms.Label();
             this.btnClearCache = new System.Windows.Forms.Button();
+            this.tabPageSettings = new System.Windows.Forms.TabPage();
+            this.gbxClearpassStartPageUrl = new System.Windows.Forms.GroupBox();
+            this.txtClearpassStartPageUrl = new System.Windows.Forms.TextBox();
+            this.gbxClearpassCredentials = new System.Windows.Forms.GroupBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.tabControl1.SuspendLayout();
             this.tabPageWifiCache.SuspendLayout();
             this.tabPageSettings.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            this.gbxClearpassStartPageUrl.SuspendLayout();
+            this.gbxClearpassCredentials.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -65,63 +69,6 @@
             this.tabPageWifiCache.Text = "WifiCache";
             this.tabPageWifiCache.UseVisualStyleBackColor = true;
             // 
-            // tabPageSettings
-            // 
-            this.tabPageSettings.Controls.Add(this.groupBox1);
-            this.tabPageSettings.Location = new System.Drawing.Point(4, 22);
-            this.tabPageSettings.Name = "tabPageSettings";
-            this.tabPageSettings.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageSettings.Size = new System.Drawing.Size(479, 301);
-            this.tabPageSettings.TabIndex = 1;
-            this.tabPageSettings.Text = "Settings";
-            this.tabPageSettings.UseVisualStyleBackColor = true;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.lblPassword);
-            this.groupBox1.Controls.Add(this.lblUsername);
-            this.groupBox1.Controls.Add(this.txtPassword);
-            this.groupBox1.Controls.Add(this.txtUsername);
-            this.groupBox1.Location = new System.Drawing.Point(6, 6);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(128, 105);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Clearpass credentials";
-            // 
-            // txtUsername
-            // 
-            this.txtUsername.Location = new System.Drawing.Point(6, 36);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(100, 20);
-            this.txtUsername.TabIndex = 0;
-            // 
-            // txtPassword
-            // 
-            this.txtPassword.Location = new System.Drawing.Point(6, 75);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.PasswordChar = '•';
-            this.txtPassword.Size = new System.Drawing.Size(100, 20);
-            this.txtPassword.TabIndex = 1;
-            // 
-            // lblUsername
-            // 
-            this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(7, 20);
-            this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(55, 13);
-            this.lblUsername.TabIndex = 2;
-            this.lblUsername.Text = "Username";
-            // 
-            // lblPassword
-            // 
-            this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(6, 59);
-            this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(53, 13);
-            this.lblPassword.TabIndex = 3;
-            this.lblPassword.Text = "Password";
-            // 
             // btnClearCache
             // 
             this.btnClearCache.Location = new System.Drawing.Point(6, 6);
@@ -132,6 +79,84 @@
             this.btnClearCache.UseVisualStyleBackColor = true;
             this.btnClearCache.Click += new System.EventHandler(this.btnClearCache_Click);
             // 
+            // tabPageSettings
+            // 
+            this.tabPageSettings.Controls.Add(this.gbxClearpassStartPageUrl);
+            this.tabPageSettings.Controls.Add(this.gbxClearpassCredentials);
+            this.tabPageSettings.Location = new System.Drawing.Point(4, 22);
+            this.tabPageSettings.Name = "tabPageSettings";
+            this.tabPageSettings.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageSettings.Size = new System.Drawing.Size(479, 301);
+            this.tabPageSettings.TabIndex = 1;
+            this.tabPageSettings.Text = "Settings";
+            this.tabPageSettings.UseVisualStyleBackColor = true;
+            // 
+            // gbxClearpassStartPageUrl
+            // 
+            this.gbxClearpassStartPageUrl.Controls.Add(this.txtClearpassStartPageUrl);
+            this.gbxClearpassStartPageUrl.Location = new System.Drawing.Point(6, 117);
+            this.gbxClearpassStartPageUrl.Name = "gbxClearpassStartPageUrl";
+            this.gbxClearpassStartPageUrl.Size = new System.Drawing.Size(217, 47);
+            this.gbxClearpassStartPageUrl.TabIndex = 1;
+            this.gbxClearpassStartPageUrl.TabStop = false;
+            this.gbxClearpassStartPageUrl.Text = "Clearpass Start Page Url";
+            // 
+            // txtClearpassStartPageUrl
+            // 
+            this.txtClearpassStartPageUrl.Location = new System.Drawing.Point(6, 19);
+            this.txtClearpassStartPageUrl.Name = "txtClearpassStartPageUrl";
+            this.txtClearpassStartPageUrl.Size = new System.Drawing.Size(202, 20);
+            this.txtClearpassStartPageUrl.TabIndex = 0;
+            this.txtClearpassStartPageUrl.Text = "https://10.1.10.203/tips/tipsLogin.action";
+            // 
+            // gbxClearpassCredentials
+            // 
+            this.gbxClearpassCredentials.Controls.Add(this.lblPassword);
+            this.gbxClearpassCredentials.Controls.Add(this.lblUsername);
+            this.gbxClearpassCredentials.Controls.Add(this.txtPassword);
+            this.gbxClearpassCredentials.Controls.Add(this.txtUsername);
+            this.gbxClearpassCredentials.Location = new System.Drawing.Point(6, 6);
+            this.gbxClearpassCredentials.Name = "gbxClearpassCredentials";
+            this.gbxClearpassCredentials.Size = new System.Drawing.Size(128, 105);
+            this.gbxClearpassCredentials.TabIndex = 0;
+            this.gbxClearpassCredentials.TabStop = false;
+            this.gbxClearpassCredentials.Text = "Clearpass credentials";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(6, 59);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(7, 20);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(55, 13);
+            this.lblUsername.TabIndex = 2;
+            this.lblUsername.Text = "Username";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(6, 75);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '•';
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.Text = "Hech0Fvlfnkjy2100ckrceT6";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(6, 36);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 0;
+            this.txtUsername.Text = "y.tretyakov";
+            // 
             // SupportToolsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -140,11 +165,14 @@
             this.Controls.Add(this.tabControl1);
             this.Name = "SupportToolsForm";
             this.Text = "Support Tools";
+            this.Deactivate += new System.EventHandler(this.SupportToolsForm_Deactivate);
             this.tabControl1.ResumeLayout(false);
             this.tabPageWifiCache.ResumeLayout(false);
             this.tabPageSettings.ResumeLayout(false);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.gbxClearpassStartPageUrl.ResumeLayout(false);
+            this.gbxClearpassStartPageUrl.PerformLayout();
+            this.gbxClearpassCredentials.ResumeLayout(false);
+            this.gbxClearpassCredentials.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -154,12 +182,15 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPageWifiCache;
         private System.Windows.Forms.TabPage tabPageSettings;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox gbxClearpassCredentials;
         private System.Windows.Forms.Label lblPassword;
         private System.Windows.Forms.Label lblUsername;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.TextBox txtUsername;
         private System.Windows.Forms.Button btnClearCache;
+        private System.Windows.Forms.GroupBox gbxClearpassStartPageUrl;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox txtClearpassStartPageUrl;
     }
 }
 
--- SupportTools/Form1.cs
+++ SupportTools/Form1.cs
@@ -7,19 +7,136 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace SupportTools
 {
     public partial class SupportToolsForm : Form
     {
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow")]
+        private static extern int FindWindow(string sClass, string sWindow);
+
+        WebBrowser Browser;
+        bool IsPageLoaded;
+
+
         public SupportToolsForm()
         {
             InitializeComponent();
-        }
+            IsPageLoaded = false;
 
+        }
+        
         private void btnClearCache_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Thread WorkThread = new Thread(new ThreadStart(WorkThreadJob));
+                WorkThread.SetApartmentState(ApartmentState.STA);
+                WorkThread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
 
+
+        public void WorkThreadJob()
+        {
+        //    RunBrowserThread(new Uri(txtClearpassStartPageUrl.Text));
+
+            Browser = new WebBrowser();
+            Browser.ScriptErrorsSuppressed = false;
+            Browser.DocumentCompleted += BrowserDocumentCompleted;
+            Browser.Navigate(txtClearpassStartPageUrl.Text);
+
+            
+
+            Wait(10);
+
+            MakeActionById("username", "SetAttribute", "value", txtUsername.Text, null);
+            MakeActionById("password", "SetAttribute", "value", txtPassword.Text, null);
+            MakeActionById("submit_btn", "InvokeMember", null, null, "Click");
+
+            Wait(10);
+
+            HtmlDocument doc = Browser.Document;
+
+
+
+        }
+
+        public void AcceptSslCertificate()
+        {
+            int nWinHandle = FindWindow(null, "Security Alert");
+            if (nWinHandle != 0)
+            {
+                SendKeys.Send("Y%");
+            }
+        }
+
+        public void MakeActionById (string elementId, string Action, string AttributeName, string AttributeValue, string InvokeMethod)
+        {
+            HtmlElement element = Browser.Document.GetElementById(elementId);
+            if (element != null)
+            {
+                switch (Action)
+                {
+                    case "SetAttribute":
+                        element.SetAttribute(AttributeName, AttributeValue);
+                        break;
+                    case "InvokeMember":
+                        element.InvokeMember(InvokeMethod);
+                        break;
+                    default:
+                        MessageBox.Show("Undefined Actopn: " + Action);
+                        break;
+                }
+            }
+        }
+
+        public void Wait (int Interval)
+        {
+            while (!IsPageLoaded)
+            {
+                Application.DoEvents();
+                Thread.Sleep(Interval);
+            }
+            IsPageLoaded = false;
+        }
+
+/*
+        private void RunBrowserThread(Uri url)
+        {
+            var th = new Thread(() => {
+                Browser = new WebBrowser();
+                Browser.ScriptErrorsSuppressed = true;
+                Browser.DocumentCompleted += BrowserDocumentCompleted;
+                Browser.Navigate(url);
+                Application.Run();
+            });
+            th.SetApartmentState(ApartmentState.STA);
+            th.Start();
+        }
+*/
+        void BrowserDocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            var br = sender as WebBrowser;
+            if (br.Url == e.Url)
+            {
+                IsPageLoaded = true;
+  
+    //          Application.ExitThread();   // Stops the thread
+            }
+        }
+
+        private void SupportToolsForm_Deactivate(object sender, EventArgs e)
+        {
+            AcceptSslCertificate();
         }
     }
 }
